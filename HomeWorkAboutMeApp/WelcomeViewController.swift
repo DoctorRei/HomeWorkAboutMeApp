bdb999d62a136c6e23f0891900513ae392f45902
AkiraRei <Akirarei@yandex.ru>
2023-06-06 19:34:06 +0300

    Views create

---
+++ HomeWorkAboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeUserViewController.swift
+//  HomeWorkAboutMeApp
+//
+//  Created by Акира on 06.06.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
