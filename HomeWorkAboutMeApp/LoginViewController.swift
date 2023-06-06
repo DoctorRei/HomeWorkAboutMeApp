22333591463492b17eabbc57842b06dd3decbdb6
AkiraRei <Akirarei@yandex.ru>
2023-06-06 19:56:53 +0300

    IB outlets and actions

--- HomeWorkAboutMeApp/LoginViewController.swift
+++ HomeWorkAboutMeApp/LoginViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet var buttonLogin:  UIButton!
+    @IBOutlet var buttonForgotUser: UIButton!
+    @IBOutlet var buttonForgotPassword: UIButton!
+    @IBOutlet var textFieldUserLogin: UITextField!
+    @IBOutlet var textFieldUserPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    // MARK: IBActions
+    @IBAction func userLoginWillBeTapped() {
+    }
+    
+    @IBAction func userForgotLoginWillBeTapped() {
+    }
+    
+    @IBAction func userForgotPasswordWillBeTapped() {
+    }
+    
 
 }
 
