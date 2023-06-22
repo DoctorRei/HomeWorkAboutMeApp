432d6aea18fe02571fcca67b92aed57267e3e6e3
AkiraRei <Akirarei@yandex.ru>
2023-06-22 18:09:12 +0300

    Ok, last func for hide keyboard and constraints

--- HomeWorkAboutMeApp/LoginViewController.swift
+++ HomeWorkAboutMeApp/LoginViewController.swift
@@ -20,8 +20,10 @@ class LoginViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         guard let welcomeVC =
-                segue.destination as? WelcomeViewController else { return } // здесь проверяем, что экран на который мы переходим ис WelcomeViewController
-        welcomeVC.userName = textFieldUserLogin.text ?? "" // lелаем лейбл, а не текстфилд
+                segue.destination as? WelcomeViewController else { return }
+        // здесь проверяем, что экран на который мы переходим ис WelcomeViewController
+        welcomeVC.userName = textFieldUserLogin.text ?? ""
+        // Делаем лейбл, а не текстфилд
         return
     }
     
@@ -30,21 +32,31 @@ class LoginViewController: UIViewController {
         let userCheckLogin = textFieldUserLogin.text
         let userCheckPassword = textFieldUserPassword.text
         
-        if userCheckLogin == UserFirst.userName && userCheckPassword == UserFirst.userPassword {
+        if userCheckLogin == UserFirst.userName &&
+            userCheckPassword == UserFirst.userPassword {
             performSegue(withIdentifier: "showWelcomeVC", sender: nil)
         } else {
-            presentAlert(withTitle: "Sorry", message: "Your login or password is wrong")
+            presentAlert(withTitle: "Sorry",
+                         message: "Your login or password is wrong")
         }
     }
     
     @IBAction func userForgotLoginWillBeTapped() {
-        presentAlert(withTitle: "C'mon!", message: "Your user name is *User*")
+        presentAlert(withTitle: "C'mon!",
+                     message: "Your user name is *User*")
     }
     
     @IBAction func userForgotPasswordWillBeTapped() {
-        presentAlert(withTitle: "C'mon!", message: "Your password is *123321*")
+        presentAlert(withTitle: "C'mon!",
+                     message: "Your password is *123321*")
     }
     
+    @IBAction func unwindToLogin( unwindSegue: UIStoryboardSegue) {
+        textFieldUserLogin.text = ""
+        textFieldUserPassword.text = ""
+    }
+    
+    // Я приехал из отпуска и примерно 2-3 недели не прикосался к коду. К огромному сожалению. До всего допер сам, но вот вопрос к этому экшену. Почему у меня напротив unwindToLogin не стоит точка (слева), которая говорит, что у меня кнопка к чему-то привязана? Т.е напротив кнопок "забыл пароль" и т.д проставлено все, но с анваиндом этого нет. При этом все замечательно работает и дай бог здоровья этому методу. Даже текстфилды стираются. https://sun9-7.userapi.com/impg/TfnDz-wyXg69pHti66rujr0SA_nkbRbnbHswbg/Dzi9K9iAfEU.jpg?size=1140x259&quality=96&sign=0e25501d29ea06dbf4a668f41dafed43&type=album
     
 }
 
--- HomeWorkAboutMeApp/WelcomeViewController.swift
+++ HomeWorkAboutMeApp/WelcomeViewController.swift
@@ -17,14 +17,4 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         labelGreetings.text = (labelGreetings.text ?? "") + userName + " \u{1F496}"
     }
-    
-    @IBAction func logOutPressed() {
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let logOut = segue.source as? LoginViewController else { return }
-        logOut.textFieldUserLogin.text = ""
-        logOut.textFieldUserPassword.text = ""
-    }
-    
 }
